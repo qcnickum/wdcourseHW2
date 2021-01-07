5e0a42a1ff52f2c31e72f4e244fa50c69f7cd110
[Quinton Nickum] <[q.c.nickum@gmail.com]>
2021-01-07 00:41:54 -0600

    section2completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import notes from "../notes";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
   return (
     <div>
-    
+      <Header />
+      <Footer />
+      <d1>{notes.map(Note)}</d1>
     </div>
   );
 }
